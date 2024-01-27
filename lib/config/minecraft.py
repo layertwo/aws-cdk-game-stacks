5ebafaafafd79971669a42a35d0a28da5a3f422b
Lucas Messenger <1335960+layertwo@users.noreply.github.com>
2024-01-26 17:07:00 -0800

    chore: bump versions and plugins

--- lib/config/minecraft.py
+++ lib/config/minecraft.py
@@ -2,8 +2,9 @@ from lib.config import GamePort, GameProperties, PortType
 
 EMAIL = "aws+minecraft@layertwo.dev"
 
+MC_VERSION = "1.20.4"
 ESSENTIALS_VER = "2.20.1"
-VOXEL_SNIPER_VER = "8.12.4"
+VOXEL_SNIPER_VER = "8.13.0"
 MV_CORE = "4.3.12"
 MV_INV = "4.2.6"
 
@@ -14,7 +15,7 @@ MODS = [
     f"https://github.com/EssentialsX/Essentials/releases/download/{ESSENTIALS_VER}/EssentialsXSpawn-{ESSENTIALS_VER}.jar",
     # World Management
     f"https://github.com/KevinDaGame/VoxelSniper-Reimagined/releases/download/v{VOXEL_SNIPER_VER}/voxelsniper-{VOXEL_SNIPER_VER}-spigot.jar",
-    "https://mediafilez.forgecdn.net/files/4586/220/worldedit-bukkit-7.2.15.jar",
+    "https://mediafilez.forgecdn.net/files/4954/406/worldedit-bukkit-7.2.18-dist.jar",
     "https://mediafilez.forgecdn.net/files/4675/318/worldguard-bukkit-7.0.9-dist.jar",
     "https://github.com/layertwo/WeatherMan/releases/download/2.0.0/WeatherMan-2.0.0.jar",
     "https://github.com/layertwo/SpigotPing/releases/download/2.7.0/SpigotPing-2.7.0.jar",
@@ -22,14 +23,14 @@ MODS = [
     f"https://github.com/Multiverse/Multiverse-Core/releases/download/{MV_CORE}/multiverse-core-{MV_CORE}.jar",
     f"https://github.com/Multiverse/Multiverse-Inventories/releases/download/{MV_INV}/multiverse-inventories-{MV_INV}.jar",
     # Permissions
-    "https://download.luckperms.net/1515/bukkit/loader/LuckPerms-Bukkit-5.4.102.jar",
+    "https://download.luckperms.net/1530/bukkit/loader/LuckPerms-Bukkit-5.4.117.jar",
     "https://mediafilez.forgecdn.net/files/3007/470/Vault.jar",
     # Transportation
-    "https://ci.mg-dev.eu/job/TrainCarts/1418/artifact/target/TrainCarts-1.20.1-v2-1418.jar",
-    "https://ci.mg-dev.eu/job/BKCommonLib/1613/artifact/build/BKCommonLib-1.20.1-v3-1613.jar",
+    "https://ci.mg-dev.eu/job/TrainCarts/1518/artifact/build/TrainCarts-1.20.4-v1-1518.jar",
+    "https://ci.mg-dev.eu/job/BKCommonLib/1663/artifact/build/BKCommonLib-1.20.4-v1-1663.jar",
     # World viewing
     # "https://cdn.modrinth.com/data/YMXhf1UJ/versions/Aziw81Ii/Pl3xMap-1.20.1-469.jar",
-    "https://github.com/DecentSoftware-eu/DecentHolograms/releases/download/2.8.3/DecentHolograms-2.8.3.jar",
+    "https://github.com/DecentSoftware-eu/DecentHolograms/releases/download/2.8.6/DecentHolograms-2.8.6.jar",
     # Monitoring - AWS
     "https://github.com/layertwo/Spigot-Cloudwatch/releases/download/v1.2.0/CloudWatch-1.2.0.jar",
 ]
@@ -43,7 +44,7 @@ MINECRAFT_PROPS = GameProperties(
     environment={
         "TYPE": "PAPER",
         "EULA": "TRUE",
-        "VERSION": "1.20.1",
+        "VERSION": MC_VERSION,
         "MAX_MEMORY": f"{MAX_MEMORY}M",
         "MAX_TICK_TIME": "60000",
         "MODS": ",".join(MODS),
