7ff20ff34692e4bbd042db15cbfce7e848740564
Lucas Messenger <1335960+layertwo@users.noreply.github.com>
2026-03-22 18:56:35 -0700

    test: add fargate_game_properties fixture

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from lib.config import GamePort, GameProperties, PortType
+from lib.config import GamePort, GameProperties, PortType, ServiceType
 
 
 @pytest.fixture
@@ -17,10 +17,28 @@ def game_properties() -> GameProperties:
             "FOO": "BAR",
         },
         auto_start=True,
-        start_time="0 23 * * FRI",  # Friday 3PM PST
-        stop_time="0 6 * * MON",  # Sunday 10PM PST,
+        start_time="0 23 * * FRI",
+        stop_time="0 6 * * MON",
         domain_name="example.com",
         hosted_zone_id="Z00000000000000000000",
         instance_type="t4g.large",
         instance_connect=True,
+    )
+
+
+@pytest.fixture
+def fargate_game_properties() -> GameProperties:
+    return GameProperties(
+        name="TestGame",
+        container_image="foobar-container/latest",
+        container_path="/data",
+        service_type=ServiceType.FARGATE,
+        ports=[
+            GamePort(port_type=PortType.TCP, number=80),
+        ],
+        environment={
+            "FOO": "BAR",
+        },
+        domain_name="example.com",
+        hosted_zone_id="Z00000000000000000000",
     )
